f3a924002891291a2c80a0d7a885296843ced982
Brett Constantinoff <77081808+Brett-Constantinoff@users.noreply.github.com>
2022-02-17 11:26:34 -0700

    Removes redundant includes and uncomments out print Tokens

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,13 @@
 #include "lexer/Token.h"
 #include "Timer.h"
 
-#include <iostream>
-#include <string>
-
-
-
 int main(int args, char** argv){
    
    Timer::start();
    Lexer::tokenize("test/test1.oak");
    Timer::stop();
   
-   
-   //Lexer::printTokens();
+   Lexer::printTokens();
 
    return 0;
 };  
